62668ddb698850cdf512c904bf335b0e556a63ad
SvevoF <svevo.fuser@studenti.unitn.it>
2022-12-01 18:42:26 +0100

    day1 - completed
    It's a matter of reading the file and discretising between the lines that are empty and the ones that contain an int. Add them together, chuck them in a list and use the max() function to obtain the desired result.
    The second half of the puzzle requires was solved with a loop that find the current max, adds it to a counter and removes the element from the list. At the next iteration, the new max will be different.

---
+++ Day1/day1.py
@@ -0,0 +1,24 @@
+with open("./input.txt") as f:
+	bags = f.readlines()
+
+# print(bags)
+
+total = 0
+bags_tot = []
+
+for line in bags:
+	if line != "\n":
+		total += int(line)
+	else:
+		bags_tot.append(total)
+		total = 0
+
+total = 0
+
+for i in range(3):
+	max_bag = max(bags_tot)
+	print(f"{i+1} most carrying Elf carries {max_bag} calories")
+	total += max_bag
+	bags_tot.remove(max_bag)
+
+print(total)
